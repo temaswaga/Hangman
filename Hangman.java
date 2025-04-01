b097383823001b2dba8a6e676f166ea08e7ffe06
Tjoma <temaswaga@mail.ru>
2025-04-02 00:13:35 +0300

    some little fixes

--- Hangman.java
+++ Hangman.java
@@ -13,31 +13,31 @@ public class Hangman {
                                           {"Obama", "First black president of USA"},
                                           {"Minecraft", "Cubical computer game"}};
 
-        for (int i = 0; i < 4; i++) {
-            String answer = game.GameStarter();
+        for (int i = 0; i < 5; i++) {     // 5 итерраций потому-что добавил только 5 вопросов
+            String answerYesOrNo = game.gameStarter();
 
-            if (answer.equalsIgnoreCase("yes")) {
+            if (answerYesOrNo.equalsIgnoreCase("yes")) {
                 System.out.println("\nWell, lets start!");
                 int numberOfMistakes = 0;
                 HashSet<Character> letters = new HashSet<>();
                 for (int j = 0; j < 1000; j++) {
-                    game.QuestionPrinter(i, words); // печатаю вопрос
-                    game.HangmanPrinter(numberOfMistakes); // печатаю виселицу
-                    int CorrectLettersNumber = game.WordPrinter(i, words, letters); // печатаю само слово
+                    game.questionPrinter(i, words); // печатаю вопрос
+                    game.hangmanPrinter(numberOfMistakes); // печатаю виселицу
+                    int correctLettersNumber = game.wordPrinter(i, words, letters); // печатаю слово
 
-                    Character lastInsertedLetter = game.LettersMemorizer(letters); // определяю наличие ошибки
-                    if (!game.MistakeDetector(i, words, lastInsertedLetter)) {
-                        numberOfMistakes++;
-                    };
-
-                    String WinOrLose = game.WinOrLoseDefiner(i, words, numberOfMistakes, CorrectLettersNumber); // определяю победу или проигрыш
-                    if (WinOrLose.equalsIgnoreCase("won") || WinOrLose.equalsIgnoreCase("lost")) {
+                    String winOrLose = game.winOrLoseDefiner(i, words, numberOfMistakes, correctLettersNumber); // определяю победу или проигрыш, если все буквы отгаданы или 6 ошибок соответственно
+                    if (winOrLose.equalsIgnoreCase("won") || winOrLose.equalsIgnoreCase("lost")) {
                         break;
                     }
+
+                    Character lastInsertedLetter = game.lettersMemorizer(letters); // определяю наличие ошибки и добавляю последний введенный символ в HashSet
+                    if (!game.mistakeDetector(i, words, lastInsertedLetter)) {
+                        numberOfMistakes++;
+                    };
                 }
             }
 
-            else if (answer.equalsIgnoreCase("no")) {
+            else if (answerYesOrNo.equalsIgnoreCase("no")) {
                 System.out.println("Bye bye!");
                 break;
             }
@@ -50,60 +50,61 @@ public class Hangman {
 
     }
 
-    public String GameStarter() {
-        System.out.println("Do you want to play? (yes/no)");
-        String answer = scanner.nextLine();
-        return answer;
+    public String gameStarter() {
+        System.out.println("Do you wanna to play? (yes/no)");
+        String answerYesOrNo = scanner.nextLine();
+        return answerYesOrNo;
     }
 
-    public void QuestionPrinter(int indexOfQuestion, String[][] words) {
+    public void questionPrinter(int indexOfQuestion, String[][] words) {
         System.out.println("Yours question is: " + words[indexOfQuestion][1]);
     }
 
-    public int WordPrinter(int indexOfWord, String[][] words, HashSet<Character> letters) {
+    public int wordPrinter(int indexOfWord, String[][] words, HashSet<Character> letters) {   // перебираю буквы секретного слова и если мой HashSet contains эту букву - вывожу её вместо "_ "
         System.out.print("      ");
-        int CorrectLettersNumber = 0;
+        int correctLettersNumber = 0;
         for (int i = 0; i < words[indexOfWord][0].length(); i++) {
             if (letters.contains(words[indexOfWord][0].charAt(i))) {
                 System.out.print(words[indexOfWord][0].charAt(i) + " ");
-                CorrectLettersNumber++;
+                correctLettersNumber++;
             }
             else {
                 System.out.print("_ ");
             }
         }
-        return CorrectLettersNumber;
+        return correctLettersNumber;
     }
 
-    public Character LettersMemorizer(HashSet<Character> letters) {
-        System.out.print("\n\n   Write any letter: ");
+    public Character lettersMemorizer(HashSet<Character> letters) {
+        System.out.print("\n\n   Enter a letter: ");
         Character lastInsertedLetter = scanner.nextLine().charAt(0);
-        letters.add(lastInsertedLetter);
+        letters.add(Character.toLowerCase(lastInsertedLetter));             // Добавляю в хешсет как нижний так и верхний регистры введенной буквы для того чтобы он был не важен
+        letters.add(Character.toUpperCase(lastInsertedLetter));
         return lastInsertedLetter;
     }
 
-    public boolean MistakeDetector(int indexOfWord, String[][] words, Character letter) {
+    public boolean mistakeDetector(int indexOfWord, String[][] words, Character letter) {
         for (int i = 0; i < words[indexOfWord][0].length(); i++) {
-            if (words[indexOfWord][0].charAt(i) == letter) {
+            if (Character.toLowerCase(letter) == Character.toLowerCase(words[indexOfWord][0].charAt(i))) {  // Приводе обе переменные к нижнему регистру чтобы избежать ошибок при сравнении
                 return true;
             }
         }
         return false;
     }
 
-    public String WinOrLoseDefiner(int indexOfWord, String[][] words, int numberOfMistakes, int CorrectLettersNumber) {
+    public String winOrLoseDefiner(int indexOfWord, String[][] words, int numberOfMistakes, int correctLettersNumber) {
         if (numberOfMistakes >= MAX_COUNT_OF_MISTAKES) {
-            System.out.println("You are looser :P");
+            System.out.println("\n\n  You are looser :P\n");
             return "lost";
         }
-        if (CorrectLettersNumber == words[indexOfWord][0].length()) {
-            System.out.println("You have won the game!!!!! :D");
+        if (correctLettersNumber == words[indexOfWord][0].length()) {
+            System.out.println("\n\n  You have won the game!!!!! :D\n");
             return "won";
         }
         return "";
     }
 
-    public void HangmanPrinter(int numberOfMistakes) {
+    public void hangmanPrinter(int numberOfMistakes) {
             if (numberOfMistakes == 0) {
                 System.out.print(ZERO_MISTAKES);
             } else if (numberOfMistakes == 1) {
