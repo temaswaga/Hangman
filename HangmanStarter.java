157831cffba44da232cf6f4864791ee28d169a80
Tjoma <temaswaga@mail.ru>
2025-04-04 21:29:02 +0300

    Delete some useless shit

--- HangmanStarter.java
+++ HangmanStarter.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class HangmanStarter  {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         HangmanGame.gamePlay();
     }
 }
